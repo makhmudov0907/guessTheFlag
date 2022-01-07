24a2eb6a6b651be3bce7e2920b8cae66e4edff44
makhmudov0907 <71522647+makhmudov0907@users.noreply.github.com>
2022-01-07 23:25:21 +0500

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Mirshod Makhmudov on 07/01/22.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
